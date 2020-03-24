ab44e9d436841d54baae79d1d65043e0b0e4c7ff
Guilherme <marquesguilherme3@gmail.com>
2020-03-24 01:01:34 -0300

    Make idx immutable

--- territory.py
+++ territory.py
@@ -1,13 +1,15 @@
-class Territory:
-    def __init__(self, idx, name, color="#FFFFFF"):
-        self.idx = idx
-        self.name = name
-        self.dom = self
-        self.color = color
-
-    def __repr__(self):
-      return f"<Territory: {self.idx}, {self.name}, dom: {self.dom.name}>"
+class Node:
+    def __init__(self, idx):
+        self._idx = idx
     
+    @property
+    def idx(self):
+        return self._idx
+    
+    @idx.setter
+    def idx(self, _):
+        self._idx = self._idx
+
     def __eq__(self, other):
         return self.idx == other.idx
     
@@ -16,3 +18,14 @@ class Territory:
     
     def __hash__(self):
         return self.idx
+
+class Territory(Node):
+    def __init__(self, idx, name, color="#FFFFFF"):
+        super().__init__(idx)
+        self.name = name
+        self.dom = self
+        self.color = color
+
+    def __repr__(self):
+      return f"<Territory: {self.idx}, {self.name}, dom: {self.dom.name}>"
+
