53c7766fbf6673f85791d0d79848f01a6bf1c5f9
Guilherme <marquesguilherme3@gmail.com>
2020-03-24 14:54:58 -0300

    more tests

--- graph/tests/graph_test.py
+++ graph/tests/graph_test.py
@@ -25,3 +25,18 @@ class TestNode:
     
     def test_get_idx_none(self, g):
         assert g.get_by_idx(50) == None
+    
+    def test_graph_is_subscriptable(self, g, init_nodes):
+        n1, *_  = init_nodes
+        g[n1]
+    
+    def test_all_nodes_stored(self, g):
+        n6 = Node(6)
+        g.add_node(n6, [])
+        assert len(g.nodes) == 6
+    
+    def test_bidirectional_links(self, g, init_nodes):
+        n6 = Node(6)
+        *_, n5 = init_nodes
+        g.add_node(n6, [n5])
+        assert n6 in g[n5]
